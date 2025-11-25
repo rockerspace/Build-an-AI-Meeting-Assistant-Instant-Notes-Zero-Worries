675053f0aaa01aa80fcbc93d77e08a70b0c7f5c1
Narendra Ragav Venkatesan <147092133+rockerspace@users.noreply.github.com>
2025-11-25 23:08:46 +0530

    Implement audio transcription using OpenAI Whisper

--- speech2text_app.py
+++ speech2text_app.py
@@ -5,10 +5,13 @@ import gradio as gr
 # Function to transcribe audio using the OpenAI Whisper model
 def transcript_audio(audio_file):
     # Initialize the speech recognition pipeline
-    pipe = #-----> Fill here <----
-
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model="openai/whisper-tiny.en",
+        chunk_length_s=30,
+            )
     # Transcribe the audio file and return the result
-    result = #-----> Fill here <----
+    result = pipe(audio_file, batch_size=8)["text"]
     return result
 
 # Set up Gradio interface
@@ -17,9 +20,9 @@ output_text = gr.Textbox()  # Text output
 
 # Create the Gradio interface with the function, inputs, and outputs
 iface = gr.Interface(fn=transcript_audio, 
-                     inputs=audio_input, outputs=output_text, 
-                     title="Audio Transcription App",
-                     description="Upload the audio file")
+        inputs=audio_input, outputs=output_text, 
+        title="Audio Transcription App",
+        description="Upload the audio file")
 
 # Launch the Gradio app
 iface.launch(server_name="0.0.0.0", server_port=5000)
